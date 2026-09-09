18de7bc52a733e6c2c1e3959f5bc757e26f3ea1d
Gilmar Santana Lins <gilmar.livecont@gmail.com>
2026-09-09 19:49:03 -0300

    Alterando local de imports no main seguindo a convenção (PEP 8) que diz  que todos os imports fiquem no topo do arquivo, antes de qualquer outro código

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI # Importando a classe FastAPI do módulo fastapi
-
-app = FastAPI(title="Biblioteca NoSQL API") # Criando uma instância da aplicação FastAPI com o título "Biblioteca NoSQL API"
-
 # Importando os routers
 from app.routers.health import health_router
 from app.routers.books import books_router
+
+
+app = FastAPI(title="Biblioteca NoSQL API") # Criando uma instância da aplicação FastAPI
+
 
 # Incluindo os routers na aplicação FastAPI
 app.include_router(health_router)
