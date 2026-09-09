922fc56cf10add8c72340b9d86262a3692f9ebff
Gilmar Santana Lins <gilmar.livecont@gmail.com>
2026-09-09 19:42:48 -0300

    Alteração de documentação

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI # Importando a classe FastAPI do módulo fastapi
 
 app = FastAPI(title="Biblioteca NoSQL API") # Criando uma instância da aplicação FastAPI com o título "Biblioteca NoSQL API"
 
-# IMportando os routers
-from routers.health import health_router
-from routers.books import books_router
+# Importando os routers
+from app.routers.health import health_router
+from app.routers.books import books_router
 
+# Incluindo os routers na aplicação FastAPI
 app.include_router(health_router)
 app.include_router(books_router)
